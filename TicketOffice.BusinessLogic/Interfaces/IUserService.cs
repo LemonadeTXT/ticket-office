c95cf07a4f9f12f205def7857e4db12ad45c8443
Stecko Anton <lemonadetxt@gmail.com>
2023-03-29 19:49:27 +0300

    add BusinessLogic class library

---
+++ TicketOffice.BusinessLogic/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using TicketOffice.Common.Dtos;
+using TicketOffice.Common.Models;
+
+namespace TicketOffice.BusinessLogic.Interfaces
+{
+    public interface IUserService
+    {
+        User Get(UserDto userDto);
+
+        List<User> GetAll();
+
+        void Create(UserDto userDto);
+
+        void Edit(UserDto userDto);
+
+        void Delete(UserDto userDto);
+    }
+}
---
+++ TicketOffice.BusinessLogic/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using TicketOffice.BusinessLogic.Interfaces;
+using TicketOffice.Common.Dtos;
+using TicketOffice.Common.Models;
+using TicketOffice.DAL;
+
+namespace TicketOffice.BusinessLogic.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationContext _applicationContext;
+
+        public UserService(ApplicationContext applicationContext)
+        {
+            _applicationContext = applicationContext;
+        }
+
+        public void Create(UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
