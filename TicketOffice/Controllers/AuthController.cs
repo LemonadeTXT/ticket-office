641e7f2d1da4e9472d1da70dae6dc6790657cae4
Stecko Anton <lemonadetxt@gmail.com>
2023-03-31 19:24:47 +0300

    added AuthController

---
+++ TicketOffice/Controllers/AuthController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using TicketOffice.Common.Dtos;
+using TicketOffice.Common.Models;
+using Microsoft.AspNetCore.Authorization;
+using TicketOffice.BusinessLogic.Interfaces;
+using AutoMapper;
+
+namespace TicketOffice.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        private readonly IUserService _userService;
+
+        public AuthController(IAuthService authService, IUserService userService)
+        {
+            _authService = authService;
+            _userService = userService;
+        }
+
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(UserAuthDto userAuthDto)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_authService.IsLogin(userAuthDto.Login, userAuthDto.Password, out int id))
+                {
+                    var user = _userService.Get(id);
+
+                    await Authenticate(user);
+
+                    return RedirectToAction("GetProfile", "Profile");
+                }
+            }
+
+            return View(userAuthDto);
+        }
+
+        public IActionResult SignUp()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserCreateDto userCreateDto)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!_authService.IsRegistration(userCreateDto.Login, userCreateDto.Email))
+                {
+                    _userService.Create(userCreateDto);
+
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+
+            return View(userCreateDto);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private async Task Authenticate(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Id.ToString()),
+                new Claim(ClaimTypes.Role, user.Role.ToString())
+            };
+
+            var claimIdentity =
+                new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimIdentity));
+        }
+    }
+}
