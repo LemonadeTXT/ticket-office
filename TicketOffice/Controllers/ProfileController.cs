f2fb566a5605ca72237a0db8e3fdd12a387db50f
Stecko Anton <lemonadetxt@gmail.com>
2023-03-31 19:25:24 +0300

    added ProfileController

---
+++ TicketOffice/Controllers/ProfileController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketOffice.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [Authorize]
+        public IActionResult GetProfile()
+        {
+            return View();
+        }
+    }
+}
