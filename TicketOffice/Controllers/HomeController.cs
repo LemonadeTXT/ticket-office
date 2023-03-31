82fd52277c35093ced94057c2314307b022067c0
Stecko Anton <lemonadetxt@gmail.com>
2023-03-31 19:25:11 +0300

    removed method

--- TicketOffice/Controllers/HomeController.cs
+++ TicketOffice/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace TicketOffice.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
