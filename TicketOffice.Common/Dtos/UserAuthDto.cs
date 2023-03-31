d4448449c8ae984dc5be6d1be97c8473aa89c472
Stecko Anton <lemonadetxt@gmail.com>
2023-03-31 19:16:42 +0300

    added attributes for properties

--- TicketOffice.Common/Dtos/UserAuthDto.cs
+++ TicketOffice.Common/Dtos/UserAuthDto.cs
@@ -1,8 +1,18 @@
-﻿namespace TicketOffice.Common.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketOffice.Common.Dtos
 {
     public class UserAuthDto
     {
+        [Required(ErrorMessage = "Please, enter login!")]
+        [StringLength(20, MinimumLength = 3,
+            ErrorMessage = "The LOGIN length must be between 3 and 20 characters!")]
         public string? Login { get; set; }
+
+        [Required(ErrorMessage = "Please, enter password!")]
+        [StringLength(20, MinimumLength = 5,
+            ErrorMessage = "The PASSWORD length must be between 5 and 20 characters!")]
+        [DataType(DataType.Password)]
         public string? Password { get; set; }
     }
 }
