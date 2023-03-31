83c3212dd0b435b9f126af50600142c23ec3e7c9
Stecko Anton <lemonadetxt@gmail.com>
2023-03-31 19:28:49 +0300

    added AuthSercice interface

---
+++ TicketOffice.BusinessLogic/Interfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketOffice.BusinessLogic.Interfaces
+{
+    public interface IAuthService
+    {
+        bool IsLogin(string login, string password, out int id);
+
+        bool IsRegistration(string login, string email);
+    }
+}
