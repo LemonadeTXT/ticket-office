28333a972ada14510b68e59e9d3c9a2dfcaf3760
Stecko Anton <lemonadetxt@gmail.com>
2023-03-31 19:24:29 +0300

    added AuthService

---
+++ TicketOffice.BusinessLogic/Services/AuthService.cs
@@ -0,0 +1,46 @@
+﻿using TicketOffice.BusinessLogic.Interfaces;
+using TicketOffice.Common.Models;
+
+namespace TicketOffice.BusinessLogic.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserService _userService;
+
+        public AuthService(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public bool IsLogin(string login, string password, out int id)
+        {
+            id = -1;
+
+            foreach (User user in _userService.GetAll())
+            {
+                if (user.Login == login &&
+                    user.Password == password)
+                {
+                    id = user.Id;
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool IsRegistration(string login, string email)
+        {
+            foreach (User user in _userService.GetAll())
+            {
+                if (user.Login == login || user.Email == email)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
