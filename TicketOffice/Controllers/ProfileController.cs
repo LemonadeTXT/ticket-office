f55428c4ab021818e73de77db0b76084a966343d
Stecko Anton <lemonadetxt@gmail.com>
2023-04-03 20:20:00 +0300

    added implementation of methods

--- TicketOffice/Controllers/ProfileController.cs
+++ TicketOffice/Controllers/ProfileController.cs
@@ -1,14 +1,62 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TicketOffice.BusinessLogic.Interfaces;
+using TicketOffice.Common.Dtos;
+using TicketOffice.Common.Models;
 
 namespace TicketOffice.Controllers
 {
     public class ProfileController : Controller
     {
-        [Authorize]
-        public IActionResult GetProfile()
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public ProfileController(IUserService userService, IMapper mapper)
         {
-            return View();
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        [Authorize]
+        public IActionResult EditProfile()
+        {
+            var user = _userService.Get(int.Parse(User.Identity.Name));
+
+            var userDto = _mapper.Map<User, UserProfileDto>(user);
+
+            return View(userDto);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult EditProfile(UserProfileDto userDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _userService.Get(int.Parse(User.Identity.Name));
+
+                if (!IsUsersEqual(userDto, user))
+                {
+                    _userService.Edit(userDto, user);
+
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return View(userDto);
+        }
+
+        private bool IsUsersEqual(UserProfileDto userDto, User user)
+        {
+            if (userDto.Email != user.Email ||
+                userDto.Login != user.Login ||
+                userDto.Password != user.Password)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
