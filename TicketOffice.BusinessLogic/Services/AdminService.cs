024b05aed0030c2beb973170bd146ecf66aa7f85
Stecko Anton <lemonadetxt@gmail.com>
2023-04-03 20:15:00 +0300

    rewrited entire implementation

--- TicketOffice.BusinessLogic/Services/AdminService.cs
+++ TicketOffice.BusinessLogic/Services/AdminService.cs
@@ -6,43 +6,59 @@ using TicketOffice.DAL;
 
 namespace TicketOffice.BusinessLogic.Services
 {
-    public class UserService : IUserService
+    public class AdminService : IAdminService
     {
         private readonly ApplicationContext _applicationContext;
         private readonly IMapper _mapper;
 
-        public UserService(ApplicationContext applicationContext, IMapper mapper)
+        public AdminService(ApplicationContext applicationContext, IMapper mapper)
         {
             _applicationContext = applicationContext;
             _mapper = mapper;
         }
 
-        public User Get(int id)
+        public User GetUserDto(int id)
         {
             var user = _applicationContext.Users.FirstOrDefault(u => u.Id == id);
 
             return user;
         }
 
-        public List<User> GetAll()
+        public List<UserDto> GetAllUsersDto()
         {
             var users = _applicationContext.Users.ToList();
 
-            return users;
+            var usersDto = new List<UserDto>();
+
+            foreach (var user in users)
+            {
+                usersDto.Add(_mapper.Map<User, UserDto>(user));
+            }
+
+            return usersDto;
         }
 
-        public void Create(UserCreateDto userCreateDto)
+        public void CreateUser(UserDto userDto)
         {
-            var user = _mapper.Map<UserCreateDto, User>(userCreateDto);
+            var user = _mapper.Map<UserDto, User>(userDto);
 
             _applicationContext.Users.Add(user);
             _applicationContext.SaveChanges();
         }
 
-        public void Edit(UserProfileDto userDto, User user)
+        public void EditUser(UserDto userDto, User user)
         {
             _applicationContext.Entry(user).CurrentValues.SetValues(userDto);
             _applicationContext.SaveChanges(true);
+        }
+
+        public void DeleteUser(UserDto userDto)
+        {
+            var user = _mapper.Map<UserDto, User>(userDto);
+
+            _applicationContext.Users.Attach(user);
+            _applicationContext.Users.Remove(user);
+            _applicationContext.SaveChanges();
         }
     }
 }
