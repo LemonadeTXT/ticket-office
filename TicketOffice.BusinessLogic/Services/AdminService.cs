a92fbba99f15784a9ad16bd745bbaca7eb6ccac5
Stecko Anton <lemonadetxt@gmail.com>
2023-04-04 23:10:57 +0300

    added functional for work with tickets

--- TicketOffice.BusinessLogic/Services/AdminService.cs
+++ TicketOffice.BusinessLogic/Services/AdminService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using TicketOffice.BusinessLogic.Interfaces;
 using TicketOffice.Common.Dtos;
 using TicketOffice.Common.Models;
@@ -17,7 +18,7 @@ namespace TicketOffice.BusinessLogic.Services
             _mapper = mapper;
         }
 
-        public User GetUserDto(int id)
+        public User GetUser(int id)
         {
             var user = _applicationContext.Users.FirstOrDefault(u => u.Id == id);
 
@@ -59,6 +60,81 @@ namespace TicketOffice.BusinessLogic.Services
             _applicationContext.Users.Attach(user);
             _applicationContext.Users.Remove(user);
             _applicationContext.SaveChanges();
+        }
+
+        public Ticket GetTicket(int id)
+        {
+            var ticket = _applicationContext.Tickets.FirstOrDefault(u => u.Id == id);
+
+            return ticket;
+        }
+
+        public List<TicketDto> GetAllTicketsDto()
+        {
+            var tickets = _applicationContext.Tickets.ToList();
+
+            var ticketsDto = new List<TicketDto>();
+
+            foreach (var ticket in tickets)
+            {
+                ticketsDto.Add(_mapper.Map<Ticket, TicketDto>(ticket));
+            }
+
+            return ticketsDto;
+        }
+
+        public void CreateTicket(TicketDto ticketDto)
+        {
+            var ticket = _mapper.Map<TicketDto, Ticket>(ticketDto);
+
+            _applicationContext.Tickets.Add(ticket);
+            _applicationContext.SaveChanges();
+        }
+
+        public void EditTicket(TicketDto ticketDto, Ticket ticket)
+        {
+            _applicationContext.Entry(ticket).CurrentValues.SetValues(ticketDto);
+            _applicationContext.SaveChanges(true);
+        }
+
+        public void DeleteTicket(int id)
+        {
+            var ticket = GetTicket(id);
+
+            _applicationContext.Tickets.Attach(ticket);
+            _applicationContext.Tickets.Remove(ticket);
+            _applicationContext.SaveChanges();
+        }
+
+        public bool EqualTickets(TicketDto ticketDto, Ticket ticket)
+        {
+            if (ticketDto.EventDate != ticket.EventDate ||
+                ticketDto.EventName != ticket.EventName ||
+                ticketDto.EventType != ticket.EventType ||
+                ticketDto.NumberOfSeats != ticket.NumberOfSeats ||
+                (ticketDto.QR != Array.Empty<byte>() && ticketDto.QR != ticket.QR))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public byte[] ConvertAvatarToByteArray(HttpRequest files)
+        {
+            byte[] avatarByteArray = Array.Empty<byte>();
+
+            foreach (var file in files.Form.Files)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    file.CopyTo(memoryStream);
+
+                    avatarByteArray = memoryStream.ToArray();
+                }
+            }
+
+            return avatarByteArray;
         }
     }
 }
