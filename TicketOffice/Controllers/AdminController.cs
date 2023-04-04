2edd96f3e969fc712bbf50121d48fbe04a040ca8
Stecko Anton <lemonadetxt@gmail.com>
2023-04-04 23:17:06 +0300

    added controller methods for work with tickets

--- TicketOffice/Controllers/AdminController.cs
+++ TicketOffice/Controllers/AdminController.cs
@@ -40,14 +40,14 @@ namespace TicketOffice.Controllers
 
                 return RedirectToAction("UsersManagement");
             }
-            
+
             return View(userDto);
         }
 
         [Authorize(Roles = "Admin")]
         public IActionResult EditUser(int id)
         {
-            var user = _adminService.GetUserDto(id);
+            var user = _adminService.GetUser(id);
 
             var userDto = _mapper.Map<User, UserDto>(user);
 
@@ -60,7 +60,7 @@ namespace TicketOffice.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = _adminService.GetUserDto(userDto.Id);
+                var user = _adminService.GetUser(userDto.Id);
 
                 if (user.Role != Common.Enum.Role.Admin)
                 {
@@ -87,7 +87,76 @@ namespace TicketOffice.Controllers
         [Authorize(Roles = "Admin")]
         public IActionResult TicketsManagement()
         {
+            return View(_adminService.GetAllTicketsDto());
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult CreateTicket()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public IActionResult CreateTicket(TicketDto ticketDto)
+        {
+            if (ModelState.IsValid)
+            {
+                ticketDto.QR = _adminService.ConvertAvatarToByteArray(Request);
+
+                if (ticketDto.QR != Array.Empty<byte>())
+                {
+                    _adminService.CreateTicket(ticketDto);
+
+                    return RedirectToAction("TicketsManagement");
+                }
+            }
+
+            return View(ticketDto);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult EditTicket(int id)
+        {
+            var ticket = _adminService.GetTicket(id);
+
+            var ticketDto = _mapper.Map<Ticket, TicketDto>(ticket);
+
+            return View(ticketDto);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public IActionResult EditTicket(TicketDto ticketDto)
+        {
+            if (ModelState.IsValid)
+            {
+                ticketDto.QR = _adminService.ConvertAvatarToByteArray(Request);
+
+                var ticket = _adminService.GetTicket(ticketDto.Id);
+
+                if (!_adminService.EqualTickets(ticketDto, ticket))
+                {
+                    if (ticketDto.QR == Array.Empty<byte>())
+                    {
+                        ticketDto.QR = ticket.QR;
+                    }
+
+                    _adminService.EditTicket(ticketDto, ticket);
+
+                    return RedirectToAction("TicketsManagement");
+                }
+            }
+
+            return View(ticketDto);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult DeleteTicket(int id)
+        {
+            _adminService.DeleteTicket(id);
+
+            return RedirectToAction("TicketsManagement");
         }
     }
 }
