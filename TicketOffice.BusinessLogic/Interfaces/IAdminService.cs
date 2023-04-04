7cfe1f15db3ba37ef276db79e09770f20f670f73
Stecko Anton <lemonadetxt@gmail.com>
2023-04-04 23:11:25 +0300

    added new methods for tickets

--- TicketOffice.BusinessLogic/Interfaces/IAdminService.cs
+++ TicketOffice.BusinessLogic/Interfaces/IAdminService.cs
@@ -1,11 +1,12 @@
 ﻿using TicketOffice.Common.Dtos;
 using TicketOffice.Common.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace TicketOffice.BusinessLogic.Interfaces
 {
     public interface IAdminService
     {
-        User GetUserDto(int id);
+        User GetUser(int id);
 
         List<UserDto> GetAllUsersDto();
 
@@ -14,5 +15,19 @@ namespace TicketOffice.BusinessLogic.Interfaces
         void EditUser(UserDto userDto, User user);
 
         void DeleteUser(UserDto userDto);
+
+        Ticket GetTicket(int id);
+
+        List<TicketDto> GetAllTicketsDto();
+
+        void CreateTicket(TicketDto ticketDto);
+
+        void EditTicket(TicketDto ticketDto, Ticket ticket);
+
+        void DeleteTicket(int id);
+
+        bool EqualTickets(TicketDto ticketDto, Ticket ticket);
+
+        byte[] ConvertAvatarToByteArray(HttpRequest files);
     }
 }
