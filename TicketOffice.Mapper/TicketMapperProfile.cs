3dedb4f256087afa680f37924d9a8a7a6badb7bc
Stecko Anton <lemonadetxt@gmail.com>
2023-04-04 23:09:24 +0300

    added mappers for users and tickets

---
+++ TicketOffice.Mapper/TicketMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TicketOffice.Common.Dtos;
+using TicketOffice.Common.Models;
+
+namespace TicketOffice.Mapper
+{
+    public class TicketMapperProfile : Profile
+    {
+        public TicketMapperProfile()
+        {
+            CreateMap<TicketDto, Ticket>().ReverseMap();
+        }
+    }
+}
---
+++ TicketOffice.Mapper/UserMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TicketOffice.Common.Dtos;
+using TicketOffice.Common.Models;
+
+namespace TicketOffice.Mapper
+{
+    public class UserMapperProfile : Profile
+    {
+        public UserMapperProfile()
+        {
+            CreateMap<UserCreateDto, User>().ReverseMap();
+            CreateMap<UserProfileDto, User>().ReverseMap();
+            CreateMap<UserDto, User>().ReverseMap();
+        }
+    }
+}
