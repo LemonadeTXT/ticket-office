ed7557ee2aa2bfb9e93881039bda5aaecc5fa26b
Stecko Anton <lemonadetxt@gmail.com>
2023-04-06 20:07:20 +0300

    added interface

---
+++ TicketOffice.BusinessLogic/Interfaces/ITicketService.cs
@@ -0,0 +1,20 @@
+﻿using TicketOffice.Common.Dtos;
+using TicketOffice.Common.Models;
+
+namespace TicketOffice.BusinessLogic.Interfaces
+{
+    public interface ITicketService
+    {
+        Ticket Get(int ticketId);
+
+        List<TicketDto> GetAllTicketsDto();
+
+        List<TicketDto> FindTickets(TicketsFindDto ticketsFindDto);
+
+        List<TicketDto> GetAllPurchasedTickets(int userId);
+
+        void PurchaseTicket(int userId, int ticketId);
+
+        bool IsPurchasedTicket(int userId, int ticketId);
+    }
+}
