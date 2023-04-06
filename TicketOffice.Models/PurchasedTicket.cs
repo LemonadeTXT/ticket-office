10b9b1066006806714b03622a1194b6b885e823d
Stecko Anton <lemonadetxt@gmail.com>
2023-04-06 20:03:49 +0300

    added new model

---
+++ TicketOffice.Models/PurchasedTicket.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketOffice.Models
+{
+    public class PurchasedTicket
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int TicketId { get; set; }
+    }
+}
