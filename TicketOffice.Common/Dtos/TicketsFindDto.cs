b1523838f3b1455bf9ca2719a44e0ef98773b8e5
Stecko Anton <lemonadetxt@gmail.com>
2023-04-06 20:04:02 +0300

    added new dto

---
+++ TicketOffice.Common/Dtos/TicketsFindDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketOffice.Common.Dtos
+{
+    public class TicketsFindDto
+    {
+        [DataType(DataType.Date)]
+        public DateTime EventDate { get; set; }
+
+        [StringLength(30,
+            ErrorMessage = "The EVENT NAME is to big, please change it!")]
+        public string? EventName { get; set; }
+
+        [Range(1, 100, ErrorMessage = "The value must be beetwen 1 and 100!")]
+        public int? NumberOfSeats { get; set; }
+    }
+}
