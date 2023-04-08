aa3bf4cbc32c7328381e738bdf86852fd88d4ab2
Stecko Anton <lemonadetxt@gmail.com>
2023-04-08 19:44:42 +0300

    added service and his interface

---
+++ TicketOffice.BusinessLogic/Interfaces/IImageService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TicketOffice.BusinessLogic.Interfaces
+{
+    public interface IImageService
+    {
+        byte[] ConvertAvatarToByteArray(HttpRequest files);
+    }
+}
---
+++ TicketOffice.BusinessLogic/Services/ImageService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using TicketOffice.BusinessLogic.Interfaces;
+
+namespace TicketOffice.BusinessLogic.Services
+{
+    public class ImageService : IImageService
+    {
+        public byte[] ConvertAvatarToByteArray(HttpRequest files)
+        {
+            byte[] avatarByteArray = Array.Empty<byte>();
+
+            foreach (var file in files.Form.Files)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    file.CopyTo(memoryStream);
+
+                    avatarByteArray = memoryStream.ToArray();
+                }
+            }
+
+            return avatarByteArray;
+        }
+    }
+}
